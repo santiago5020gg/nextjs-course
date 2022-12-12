2f9931fe6e533003e4695d344d3bec6f8f1eb603
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-12-11 20:48:40 -0500

    change timer of nodejs

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -1,12 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { setTimeout } from "timers/promises";
 import { MoviesConstant } from "../../constants/movies";
 
 
 const data = async () => {
   const response = MoviesConstant;
-  await setTimeout(2000);
+  // await setTimeout(2000);
   return response;
 }
 
