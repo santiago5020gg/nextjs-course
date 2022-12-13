0c271cbf5c9e013ca7447c6ed883c92bcd958b55
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-12-13 14:00:21 -0500

    fix import

--- pages/api/movies.ts
+++ pages/api/movies.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { movies } from "../../lib/mongodb/movie";
+import { getMoviesFromMongoDb } from "../../lib/mongodb/movie";
 import connectMongoDb from "../../models/services/mongodb/config";
 
 type Data = {
@@ -14,9 +14,8 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    const db = await connectMongoDb();
     if (req.method === "GET") {
-      const allMovies = await movies(db);
+      const allMovies = await getMoviesFromMongoDb();
       res.status(200).json(allMovies);
     }
   } catch (error:any) {
