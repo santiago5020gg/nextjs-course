3c56aa10967c0e21b9318d23c748701d2f9d16bd
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-12-29 14:02:45 -0500

    config jest in nextjs

--- constants/hero/index.tsx
+++ constants/hero/index.tsx
@@ -1,4 +1,4 @@
 import { HeroType } from "../../models/interfaces/hero";
 import HeroJson from "../hero/index.json";
 
-export const HeroConstant: HeroType[] = HeroJson as unknown as HeroType[];
+export const HeroConstant: HeroType = HeroJson;
