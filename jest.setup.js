5eb0882ee8da91a464fd7953d3dd5711a15ede64
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-12-29 15:38:22 -0500

    Fetch config process

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
-import { TextEncoder, TextDecoder } from 'util'
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,19 +1,42 @@
 import { render, screen } from "@testing-library/react";
 import Home from "./index";
-import {Movie} from '../models/interfaces/movie';
-import {HeroType} from '../models/interfaces/hero';
-import { MoviesConstant } from '../constants/movies/index';
+import { Movie } from "../models/interfaces/movie";
+import { HeroType } from "../models/interfaces/hero";
+import { Plan } from "../models/interfaces/plans";
+import { MoviesConstant } from "../constants/movies/index";
 import { HeroConstant } from "../constants/hero";
-import { setupWorker, rest } from 'msw';
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { PlansConstant } from "../constants/plans";
 
 const movieListMock: Movie[] = MoviesConstant;
 const heroMock: HeroType = HeroConstant;
+const plansMock: Plan[] = PlansConstant;
+
+const server = setupServer(
+  // Describe network behavior with request handlers.
+  // Tip: move the handlers into their own module and
+  // import it across your browser and Node.js setups!
+  rest.get("/api/plans/2", (req, res, ctx) => {
+    return res(ctx.json(plansMock));
+  })
+);
+
 
 beforeAll(() => {
-  render(<Home moviesList={movieListMock} hero={heroMock} />);
+  server.listen();
 });
 
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
 describe("Home", () => {
+  beforeAll(() => {
+    render(<Home moviesList={movieListMock} hero={heroMock} />);
+  });
+
   it("should renders show all plans", () => {
     const contactElement = screen.getByText(/show all plans/i);
     expect(contactElement).toBeInTheDocument();
