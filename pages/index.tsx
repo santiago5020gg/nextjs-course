7e624512ec4ceceea095d96c402708a4d6e5b314
OAINTERNAL\santiago.restrepo-bu <santiago-restrepo.burgos@overactive.com>
2022-12-30 14:01:09 -0500

    remove dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ const Home = ({
     } catch (error) {
       console.log("Something went wrong. getInitPlans", error);
     }
-  }, [getUrlPlan, planContext?.numberClicks]);
+  }, [getUrlPlan]);
 
   useEffect(() => {
     getInitPlans();
